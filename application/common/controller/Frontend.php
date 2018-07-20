7e66dfa95a26aa13341040b2f5e2639ea07e7eb6
Lance-93 <glen92093@gmail.com>
2018-07-20 14:39:17 +0800

    配置微信回调，用户授权

--- application/common/controller/Frontend.php
+++ application/common/controller/Frontend.php
@@ -31,6 +31,14 @@ class Frontend extends Controller
      * @var array
      */
     protected $noNeedRight = [];
+    /**
+     * 微信appid
+     */
+    protected $appid=null;
+    /**
+     * 微信secret
+     */
+    protected $secret = null;
 
     /**
      * 权限Auth
@@ -40,9 +48,40 @@ class Frontend extends Controller
 
     public function _initialize()
     {
+         
+        
         //移除HTML标签
         $this->request->filter('strip_tags');
-        $modulename = $this->request->module();
+        $modulename = strtolower($this->request->module());
+
+        //获取授权
+        if($modulename=='index'){
+            $this->appid = Config::get('wechat')['APPID'];
+            $this->secret = Config::get('wechat')['APPSECRET'];
+            $token  = cache('Token');
+
+            if(!$token['access_token'] || $token['expires_in'] <= time()){
+                $rslt  = gets("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={$this->appid}&secret={$this->secret}");
+                if($rslt){
+                    $accessArr = array(
+                        'access_token'=>$rslt['access_token'],
+                        'expires_in'=>time()+$rslt['expires_in']-200
+                    );
+                    cache('Token',$accessArr) ;
+                    $token = $rslt;
+                }
+            } 
+            if(!session('MEMBER')){
+        
+                ##没有登录
+                ##如果没有登录，我们要让url地址跳转到 微信url 去获取code
+                $myurl =  urlencode('https://crm.aicheyide.com/admin/wechat/User/addUser');//mvc : http://wx4.cdphm.net/User/wxlogin  ##微信回调地址（这个地址是我们自己的一个url地址，必须使用urlencode处理）
+                $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$this->appid}&redirect_uri={$myurl}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+                header('Location:'.$url);
+                die();
+            } 
+
+        }
         $controllername = strtolower($this->request->controller());
         $actionname = strtolower($this->request->action());
 
