d5bb7b8cd21ab4e5b28a00b87d7a19d8322f3596
lqhqf422 <lqhqf422@gmail.com>
2018-07-23 18:26:05 +0800

    导入模板下载

---
+++ public/assets/js/backend/promote/import.js
@@ -0,0 +1,90 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'promote/platform/index',
+                    add_url: 'promote/platform/add',
+                    edit_url: 'promote/platform/edit',
+                    del_url: 'promote/platform/del',
+                    multi_url: 'promote/platform/multi',
+                    table: 'platform',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'name', title: __('Name')},
+                        {field: 'status', title: __('Status'), visible:false, searchList: {"normal":__('normal'),"hidden":__('hidden')}},
+                        {field: 'status_text', title: __('Status'), operate:false},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+            // 下载导入模板 
+            $(document).on("click", ".btn-download", function () {   
+                var ids = Table.api.selectedids(newCustomer);
+                var url = 'promote/customertabs/download';
+                var options = {
+                    shadeClose: false,
+                    shade: [0.3, '#393D49'],
+                    area:['30%','30%'],
+                    callback:function(value){
+                                                    
+                    }
+                }
+                Fast.api.open(url,'下载导入模板',options)
+            })
+        },
+        import:function(){
+ 
+            // $(".btn-add").data("area", ["300px","200px"]);
+            Table.api.init({
+               
+            });
+            Form.api.bindevent($("form[role=form]"), function(data, ret){
+                //这里是表单提交处理成功后的回调函数，接收来自php的返回数据
+                Fast.api.close(data);//这里是重点
+                // console.log(data);
+                // Toastr.success("成功");//这个可有可无
+            }, function(data, ret){
+                // console.log(data);
+                
+                Toastr.success("失败");
+                
+            });
+            // Controller.api.bindevent();
+            // console.log(Config.id);
+            
+ 
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
