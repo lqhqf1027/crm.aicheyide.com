e9f23f11e19faeefd8e563279c40956e49a32a5c
cyruszx <17360268104@139.com>
2018-08-13 18:44:07 +0800

    图片

--- application/admin/controller/material/Driver.php
+++ application/admin/controller/material/Driver.php
@@ -24,11 +24,12 @@ class Driver extends Backend
         parent::_initialize();
 //        $this->model = model('MortgageRegistration');
 //        $this->view->assign("genderdataList", $this->model->getGenderdataList());
+        $this->loadlang('material/driver');
     }
 
     public function index()
     {
-        $this->loadlang('backoffice/custominfotabs');
+
 
         return $this->view->fetch();
     }
@@ -56,19 +57,18 @@ class Driver extends Backend
      */
     public function edit($ids = NULL)
     {
+        $row = Db::table("crm_order_view")
+        ->where("id",$ids)
+        ->field("archival_coding,signtime,total_contract,end_money,hostdate,mortgage,mortgage_people,ticketdate,supplier,tax_amount,no_tax_amount,pay_taxesdate,house_fee,luqiao_fee,insurance_buydate,car_boat_tax,insurance_policy,commercial_insurance_policy,transferdate")
+        ->select();
+        $row = $row[0];
 
-        $this->model = model("MortgageRegistration");
-        $row = $this->model->get($ids);
-        if (!$row)
-            $this->error(__('No Results were found'));
-        $adminIds = $this->getDataLimitAdminIds();
-        if (is_array($adminIds)) {
-            if (!in_array($row[$this->dataLimitField], $adminIds)) {
-                $this->error(__('You have no permission'));
-            }
-        }
+
+
         if ($this->request->isPost()) {
             $params = $this->request->post("row/a");
+            $params['signtime'] = strtotime($params['signtime']);
+
             if ($params) {
                 try {
                     //是否采用模型验证
@@ -77,7 +77,40 @@ class Driver extends Backend
                         $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.edit' : true) : $this->modelValidate;
                         $row->validate($validate);
                     }
-                    $result = $row->allowField(true)->save($params);
+                    $doUpdate = [
+                        'archival_coding'=>$params['archival_coding'],
+                        'signtime'=>$params['signtime'],
+                        'total_contract'=>$params['total_contract'],
+                        'end_money'=>$params['end_money'],
+                        'hostdate'=>$params['hostdate'],
+                        'mortgage'=>$params['mortgage'],
+                        'mortgage_people'=>$params['mortgage_people'],
+                        'ticketdate'=>$params['ticketdate'],
+                        'supplier'=>$params['supplier'],
+                        'tax_amount'=>$params['tax_amount'],
+                        'no_tax_amount'=>$params['no_tax_amount'],
+                        'pay_taxesdate'=>$params['pay_taxesdate'],
+                        'house_fee'=>$params['house_fee'],
+                        'luqiao_fee'=>$params['luqiao_fee'],
+                        'insurance_buydate'=>$params['insurance_buydate'],
+                        'car_boat_tax'=>$params['car_boat_tax'],
+                        'insurance_policy'=>$params['insurance_policy'],
+                        'commercial_insurance_policy'=>$params['commercial_insurance_policy'],
+                        'transferdate'=>$params['transferdate']
+                    ];
+                    $res= Db::name("sales_order")
+                    ->alias("so")
+                    ->join("mortgage_registration mr","so.mortgage_registration_id=mr.id")
+                    ->where("so.id",$ids)
+                    ->field("mr.id as mrid")
+                    ->select();
+
+                    $res = $res[0]['mrid'];
+
+                    $result = Db::name("mortgage_registration")
+                    ->where("id",$res)
+                    ->update($doUpdate);
+
                     if ($result !== false) {
                         $this->success();
                     } else {
