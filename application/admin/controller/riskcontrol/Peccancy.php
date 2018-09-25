54e6bd829f9e48a2e4d52986c5670ca839d8740a
cyruszx <17360268104@139.com>
2018-09-25 14:00:35 +0800

    违章

--- application/admin/controller/riskcontrol/Peccancy.php
+++ application/admin/controller/riskcontrol/Peccancy.php
@@ -3,6 +3,7 @@
 namespace app\admin\controller\riskcontrol;
 
 use app\common\controller\Backend;
+use think\Db;
 
 /**
  * 违章信息管理
@@ -31,25 +32,80 @@ class Peccancy extends Backend
     public function sendMessage()
     {
         if ($this->request->isAjax()) {
+
             $params = input('post.')['ids'];
             $finals = [];
+            $keys = '217fb8552303cb6074f88dbbb5329be7';
             foreach ($params as $k => $v) {
                 //获取城市前缀接口
-                $result = gets("http://v.juhe.cn/sweizhang/carPre.php?key=" . $v['key'] . "&hphm=" . urlencode($v['hphm']));
+                $result = gets("http://v.juhe.cn/sweizhang/carPre.php?key=" . $keys . "&hphm=" . urlencode($v['hphm']));
 
                 if ($result['error_code'] == 0) {
 
+                    $field = array();
 
-                    $data= gets("http://v.juhe.cn/sweizhang/query?city=" . $result['result']['city_code'] . "&hphm=" . urlencode($v['hphms']) . "&engineno=" . $v['engineno'] . "&classno=" . $v['classno'] . "&key=217fb8552303cb6074f88dbbb5329be7");
+                    $data = gets("http://v.juhe.cn/sweizhang/query?city=" . $result['result']['city_code'] . "&hphm=" . urlencode($v['hphms']) . "&engineno=" . $v['engineno'] . "&classno=" . $v['classno'] . "&key=" . $keys);
 
-                    if($data['error_code'] == 0){
-                        array_push($finals,$data);
+                    if ($data['error_code'] == 0) {
+                        $total_fraction = 0;     //总扣分
+                        $total_money = 0;        //总罚款
+                        if ($data['result']['lists']) {
+                            foreach ($data['result']['lists'] as $key => $value) {
+                                if ($value['fen']) {
+                                    $value['fen'] = floatval($value['fen']);
+
+                                    $total_fraction += $value['fen'];
+                                }
+
+                                if ($value['money']) {
+                                    $value['money'] = floatval($value['money']);
+
+                                    $total_money += $value['money'];
+                                }
+
+                            }
+                            $field['peccancy_detail'] = json_encode($data['result']['lists']);
+                        }
+
+                        if (count($data['result']['lists']) > 0) {
+                            $field['peccancy_status'] = 2;
+                        } else {
+                            $field['peccancy_status'] = 1;
+                        }
+
+
+                        $field['total_deduction'] = $total_fraction;
+                        $field['total_fine'] = $total_money;
+                        $field['final_time'] = time();
+
+
+                        $query = Db::name('violation_inquiry')
+                            ->where('license_plate_number', $v['hphms'])
+                            ->field('query_times')
+                            ->find()['query_times'];
+
+                        if (!$query) {
+                            $query = 0;
+                        }
+
+                        $query = intval($query);
+
+                        $query++;
+
+                        $field['query_times'] = $query;
+
+
+                        Db::name('violation_inquiry')
+                            ->where('license_plate_number', $v['hphms'])
+                            ->update($field);
+
+                        array_push($finals, $data);
                     }
                 }
 
             }
 
-             $this->success('','',$finals);
+            $this->success('', '', $finals);
         }
     }
 
@@ -69,6 +125,31 @@ class Peccancy extends Backend
             }
             return $result['error_code'] == 0 ? $this->success($result['reason'], null, $result) : $this->error($result['reason']);
         }
+    }
+
+
+    public function details($ids = null)
+    {
+        $detail = Db::name('violation_inquiry')
+            ->where('id', $ids)
+            ->field('username,phone,peccancy_detail,total_deduction,total_fine')
+            ->find();
+
+        $details = json_decode($detail['peccancy_detail'], true);
+
+//        foreach ($detail as $k=>$v){
+//            pr($v['money']);
+//        }
+
+        $this->view->assign([
+            'detail'=>$details,
+            'phone'=>$detail['phone'],
+            'username'=>$detail['username'],
+            'total_fine'=>$detail['total_fine'],
+            'total_deduction'=>$detail['total_deduction']
+            ]);
+
+        return $this->view->fetch();
     }
 
 
