ba0554ffbfb267dc2a3067ab1c00d50e0fa2553e
skyvow <724355924@qq.com>
2018-11-20 13:03:52 +0800

    添加搜索也

---
+++ addons/cms/wxapp/page/search/index.js
@@ -0,0 +1,44 @@
+const app = getApp()
+
+Page({
+	data: {
+		new: [],
+		used: [],
+	},
+    onCancel() {
+        wx.switchTab({
+            url: '/page/preference/list/index',
+        })
+    },
+	onChange(e) {
+		console.log('onChange', e)
+		this.getList(e.detail.value)
+	},
+	getList(queryModels) {
+        if (this.timeout) {
+            clearTimeout(this.timeout)
+            this.timeout = null
+        }
+
+        if (!queryModels) {
+            this.setData({
+                new: [],
+                used: []
+            })
+            return
+        }
+
+        this.timeout = setTimeout(() => {
+            app.request('/index/searchModels', { queryModels }, (data, ret) => {
+                console.log(data)
+                this.setData({
+                    new: data && data.new,
+                    used: data && data.used,
+                })
+            }, (data, ret) => {
+                console.log(data)
+                // app.error(ret.msg)
+            })
+        }, 250)
+    },
+})
