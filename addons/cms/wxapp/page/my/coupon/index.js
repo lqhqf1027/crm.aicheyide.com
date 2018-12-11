a82e7cc9e3f59d50f46144253fabefbc34f5f44f
skyvow <724355924@qq.com>
2018-12-11 22:13:15 +0800

    修改优惠券

--- addons/cms/wxapp/page/my/coupon/index.js
+++ addons/cms/wxapp/page/my/coupon/index.js
@@ -1,9 +1,16 @@
+import { dateFormat } from '../../../utils/util'
+
 const app = getApp()
 
 Page({
     data: {
-        key: 'fabulous',
+        globalData: app.globalData,
+        list: [],
+        key: 'notUsed',
         index: 0,
+    },
+    onLoad() {
+        this.setData({ globalData: app.globalData })
     },
 	onShow() {
 		this.getList()
@@ -17,6 +24,37 @@ Page({
 	getList() {
         app.request('/my/coupons', {}, (data, ret) => {
             console.log(data)
+            const { used, notUsed, overdues } = data.coupons
+            this.setData({
+                list: [{
+                    name: '未使用',
+                    type: 'notUsed',
+                    detailed: notUsed.map((n) => {
+                        return {
+                            ...n,
+                            date: `${dateFormat(n.createtime * 1000)} - ${dateFormat(n.validity_datetime * 1000)}`,
+                        }
+                    }),
+                }, {
+                    name: '已使用',
+                    type: 'used',
+                    detailed: used.map((n) => {
+                        return {
+                            ...n,
+                            date: `${dateFormat(n.createtime * 1000)} - ${dateFormat(n.validity_datetime * 1000)}`,
+                        }
+                    }),
+                }, {
+                    name: '已过期',
+                    type: 'overdues',
+                    detailed: overdues.map((n) => {
+                        return {
+                            ...n,
+                            date: `${dateFormat(n.createtime * 1000)} - ${dateFormat(n.validity_datetime * 1000)}`,
+                        }
+                    }),
+                }],
+            })
             wx.stopPullDownRefresh()
         }, (data, ret) => {
             console.log(data)
@@ -25,7 +63,7 @@ Page({
     },
     onChange(e) {
         const { key } = e.detail
-        const index = this.data.integral.map((n) => n.type).indexOf(key)
+        const index = this.data.list.map((n) => n.type).indexOf(key)
 
         this.setData({
             key,
@@ -34,7 +72,7 @@ Page({
     },
     onSwiperChange(e) {
         const { current: index, source } = e.detail
-        const { type: key } = this.data.integral[index]
+        const { type: key } = this.data.list[index]
 
         if (!!source) {
             this.setData({
