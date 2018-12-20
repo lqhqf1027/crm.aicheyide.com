bc29ee2ab9f54a379ff222dd57ba744e88225894
Cyruszx <17360268104@139.com>
2018-12-20 17:12:03 +0800

    11

---
+++ addons/cms/model/Prize.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EDZ
+ * Date: 2018/12/20
+ * Time: 16:11
+ */
+
+namespace addons\cms\model;
+
+
+use think\Model;
+
+class Prize extends Model
+{
+   protected $name = 'cms_prize';
+}
---
+++ application/admin/lang/zh-cn/cms/prize.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Id'  =>  '优惠券ID',
+    'Prize_name'  =>  '奖品名称',
+    'Prize_image'  =>  '奖品图片',
+    'Win_prize_number'  =>  '中奖指数',
+    'Rules'  =>  '使用规则说明',
+    'Total_payment'  =>  '发放总量',
+    'Total_surplus'  =>  '剩余总量',
+    'City_id'  =>  '城市ID',
+    'Status'  =>  '状态'
+];
